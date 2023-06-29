96f17d57606bc4de5269c7128927885bd9b14ada
Egor <zinchenko02051999@gmail.com>
2023-06-29 18:11:18 +0300

    prepared project for deployment2

---
+++ migrations/versions/c290063c04a5_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c290063c04a5
+Revises: 5604cbb55c89
+Create Date: 2023-06-29 17:36:53.355108
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c290063c04a5'
+down_revision = '5604cbb55c89'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
