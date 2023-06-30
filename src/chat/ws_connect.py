1912e37c6c99e8f2d50ca7b7b0b5f08657f12fe7
Egor <zinchenko02051999@gmail.com>
2023-06-30 08:51:17 +0300

    added script to remove old messages from the screen if window is full

--- src/chat/ws_connect.py
+++ src/chat/ws_connect.py
@@ -6,8 +6,10 @@ import asyncio
 async def main():
     async with aiohttp.ClientSession() as session:
         client_id = int(time.time() * 1000)
-        # async with session.ws_connect(f'http://localhost:8000/chat/ws/{client_id}') as ws:
-        async with session.ws_connect(f'https://global-messenger.onrender.com/chat/ws/{client_id}') as ws:
+        #TODO switch to second session if deploying
+
+        async with session.ws_connect(f'http://localhost:8000/chat/ws/{client_id}') as ws:
+        # async with session.ws_connect(f'https://global-messenger.onrender.com/chat/ws/{client_id}') as ws:
             async for msg in ws:
                 if msg.type == aiohttp.WSMsgType.TEXT:
                     with open("ws_messages.txt", "a") as file:
